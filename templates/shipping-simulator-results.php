eb16008fffc9f517c7bdf142cb4f385b172fbe36
Luiz <luizpbills@gmail.com>
2022-07-13 17:39:37 -0300

    add notice when has not shipping options

--- templates/shipping-simulator-results.php
+++ templates/shipping-simulator-results.php
@@ -1,18 +1,27 @@
-<?php do_action( 'wc_shipping_simulator_results_before' ) ?>
+<?php use Shipping_Simulator\Helpers as h; ?>
 
-<table>
-	<?php foreach ( $rates as $rate ) : ?>
-		<tr class="shipping-rate-method-<?= esc_attr( $rate->get_method_id() ) ?>">
-			<th class="col-label">
-				<span class="shipping-rate-label"><?= esc_html( $rate->get_label() ); ?></span>
-				<?php do_action( 'wc_shipping_simulator_results_col_label', $rate ) ?>
-			</th>
-			<td  class="col-cost">
-				<?= wc_price( $rate->get_cost() ); ?>
-				<?php do_action( 'wc_shipping_simulator_results_col_cost', $rate ) ?>
-			</td>
-		</tr>
-	<?php endforeach; ?>
-</table>
+<?php if ( count( $rates ) ) : ?>
+	<?php do_action( 'wc_shipping_simulator_results_before' ) ?>
 
-<?php do_action( 'wc_shipping_simulator_results_after' ) ?>
+	<table>
+		<?php foreach ( $rates as $rate ) : ?>
+			<tr class="shipping-rate-method-<?= esc_attr( $rate->get_method_id() ) ?>">
+				<th class="col-label">
+					<span class="shipping-rate-label"><?= esc_html( $rate->get_label() ); ?></span>
+					<?php do_action( 'wc_shipping_simulator_results_col_label', $rate ) ?>
+				</th>
+				<td  class="col-cost">
+					<?= wc_price( $rate->get_cost() ); ?>
+					<?php do_action( 'wc_shipping_simulator_results_col_cost', $rate ) ?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+
+	<?php do_action( 'wc_shipping_simulator_results_after' ) ?>
+<?php elseif ( $no_results_notice ) : ?>
+
+	<div class="no-results"><?= h::safe_html( $no_results_notice ) ?></div>
+
+<?php endif ?>
+
