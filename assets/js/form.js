fbd8c00873526b993747feeaf81c4518a443dc97
Luiz <luizpbills@gmail.com>
2022-07-13 16:20:38 -0300

    improve js hooks

--- assets/js/form.js
+++ assets/js/form.js
@@ -23,56 +23,76 @@ window.addEventListener('DOMContentLoaded', () => {
             button.disabled = false;
             button.classList.remove('loading')
         },
+        submitHandler: (evt) => {
+            evt.preventDefault();
+    
+            const qty = getQuantity();
+            const product = getProduct();
+    
+            let formData = `action=${form.dataset.ajaxAction}&nonce=${nonce.value}&postcode=${input.value}&product=${product.id}&quantity=${qty >= 1 ? qty : 1}`;
+            
+            formData = hooks.filterFormData(formData);
+            
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', encodeURI(form.action + '?' + formData), true);
+            xhr.onreadystatechange = () => {
+                if (XMLHttpRequest.DONE !== xhr.readyState) return;
+                hooks.afterSubmit(xhr)
+                
+                const res = JSON.parse(xhr.responseText);
+                if (res.success) {
+                    // success
+                    results.innerHTML = hooks.filterResults(res.results_html ? res.results_html : '')
+                } else {
+                    // error
+                    alert(res.error)
+                }
+                console.log(xhr)
+            };
+    
+            xhr = hooks.filterXHR(xhr)
+    
+            hooks.beforeSubmit(xhr)
+
+            xhr.send();
+        },
+        inputMaskHandler: () => {
+            if (input.dataset.mask) {
+                // apply input mask
+                input.addEventListener('input', (evt) => {
+                    const mask = input.dataset.mask;
+                    input.value = applyMask(input.value || '', mask);
+                    input.maxLength = mask ? mask.length : 20;
+                })
+                
+                // usage: applyMask('01012000', 'XX-XX-XXXX') // returns "01-01-2000"
+                function applyMask (text, mask, symbol = 'X') {
+                    if (!mask) return text;
+                    let result = '';
+                    // remove all non allphanumerics
+                    const _text = (text + '').replace(/[^a-z0-9]/gi, '');
+                    for (let i = 0, j = 0, len = mask.length; i < len; i++) {
+                        if (!_text[j]) break;
+                        if (symbol === mask[i]) {
+                            result += _text[j]
+                            j++
+                        } else {
+                            result += mask[i] || '';
+                            j = j > 0 ? j-- : 0;
+                        }
+                    }
+                    return result;
+                  }
+            }
+        }
     };
 
     const event = new Event('wc_shipping_simulator:init');
     d.dispatchEvent(event);
     
-    form.addEventListener('submit', (evt) => {
-        evt.preventDefault();
+    form.addEventListener('submit', hooks.submitHandler);
 
-        const qty = getQuantity();
-        const product = getProduct();
-
-        let formData = `action=${form.dataset.ajaxAction}&nonce=${nonce.value}&postcode=${input.value}&product=${product.id}&quantity=${qty >= 1 ? qty : 1}`;
-        
-        formData = hooks.filterFormData(formData);
-        
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', encodeURI(form.action + '?' + formData), true);
-        xhr.onreadystatechange = () => {
-            if (XMLHttpRequest.DONE !== xhr.readyState) return;
-            hooks.afterSubmit(xhr)
-            
-            const res = JSON.parse(xhr.responseText);
-            if (res.success) {
-                // success
-                results.innerHTML = hooks.filterResults(res.results_html ? res.results_html : '')
-            } else {
-                // error
-                alert(res.error)
-            }
-            console.log(xhr)
-        };
-
-        xhr = hooks.filterXHR(xhr)
-
-        hooks.beforeSubmit(xhr)
-
-        button.classList.add('loading')
-        xhr.send();
-    });
-
-    if (input.dataset.mask) {
-        // apply input mask
-        input.addEventListener('input', (evt) => {
-            input.value = applyMask(input.value || '', input.dataset.mask);
-            input.maxLength = input.dataset.mask.length;
-        })
-        // helper: applyMask (text: string, mask: string, symbol: string = 'X'): string
-        // usage: applyMask('01012000', 'XX-XX-XXXX') // returns "01-01-2000"
-        function applyMask(e,t,n="X"){let l="";const o=(e+"").replace(/[^a-z0-9]/gi,"");for(let e=0,r=0,c=t.length;e<c&&o[r];e++)n===t[e]?(l+=o[r],r++):(l+=t[e]||"",r=r>0?r--:0);return l}
-    }
+    hooks.inputMaskHandler && hooks.inputMaskHandler();
 
     function getProduct () {
         const product = {
