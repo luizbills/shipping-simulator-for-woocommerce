b0124dc37c0d42d508f45e0d9e33bf6ee997b812
Luiz <luizpbills@gmail.com>
2022-07-13 13:55:32 -0300

    improve results template

--- templates/shipping-simulator-results.php
+++ templates/shipping-simulator-results.php
@@ -1,8 +1,18 @@
+<?php do_action( 'wc_shipping_simulator_results_before' ) ?>
+
 <table>
 	<?php foreach ( $rates as $rate ) : ?>
-		<tr class="method-<?= esc_attr( $rate->get_method_id() ) ?>">
-			<th class="col-label"><?= $rate->get_label(); ?></th>
-			<td  class="col-cost"><?= wc_price( $rate->get_cost() ); ?></td>
+		<tr class="shipping-rate-method-<?= esc_attr( $rate->get_method_id() ) ?>">
+			<th class="col-label">
+				<span class="shipping-rate-label"><?= esc_html( $rate->get_label() ); ?></span>
+				<?php do_action( 'wc_shipping_simulator_results_col_label', $rate ) ?>
+			</th>
+			<td  class="col-cost">
+				<?= wc_price( $rate->get_cost() ); ?>
+				<?php do_action( 'wc_shipping_simulator_results_col_cost', $rate ) ?>
+			</td>
 		</tr>
 	<?php endforeach; ?>
 </table>
+
+<?php do_action( 'wc_shipping_simulator_results_after' ) ?>
