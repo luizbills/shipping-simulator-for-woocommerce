bd1d9c3ab9651dea1e26c560b6eaca607d34ac20
Luiz <luizpbills@gmail.com>
2022-07-13 14:12:55 -0300

    improve form template

--- templates/shipping-simulator-form.php
+++ templates/shipping-simulator-form.php
@@ -1,16 +1,14 @@
 <section id="wc-shipping-sim">
 	<form id="wc-shipping-sim-form" action="<?= esc_url( $ajax_url ); ?>" data-ajax-action="<?= esc_attr( $ajax_action ) ?>" data-product-id="<?= esc_attr( $product_id ); ?>" data-product-type="<?= esc_attr( $product_type ); ?>">
-		<div id="wc-shipping-sim-before">
-			<strong>Consulte o frete e prazo de entrega:</strong>
-		</div>
-		<div>
+		<?php do_action( 'wc_shipping_simulator_form_start' ) ?>
+
+		<div id="wc-shipping-sim-form-fields">
 			<input type="text" name="postcode" placeholder="Digite seu CEP" title="Digite seu CEP" class="input-text" data-mask="<?= esc_attr( $input_mask ); ?>" required>
 			<button type="submit" class="button submit">Consultar</button>
 			<?= $nonce ?>
 		</div>
-		<div id="wc-shipping-sim-after">
-			<a href="https://buscacepinter.correios.com.br/app/endereco/index.php">Não sei meu cep</a>
-		</div>
+
+		<?php do_action( 'wc_shipping_simulator_form_end' ) ?>
 	</form>
 
 	<div id="wc-shipping-sim-results"></div>
