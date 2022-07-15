217dc8ee012f4db24ad2721aa29944e11311d108
Luiz "Bills <luizbills@pm.me>
2022-07-15 10:58:11 -0300

    use wc_correios_get_estimating_delivery

--- classes/Integration/Correios.php
+++ classes/Integration/Correios.php
@@ -35,17 +35,10 @@ final class Correios {
 			foreach ( $rates as $rate ) {
 				if ( h::str_starts_with( $rate->get_method_id(), 'correios-' ) ) {
 					$metadata = $rate->get_meta_data();
-					$delivery = absint( h::get( $metadata['_delivery_forecast'] ) );
+					$delivery = intval( h::get( $metadata['_delivery_forecast'] ) );
 					if ( $delivery > 0 ) {
-						$time = absint( $delivery ) > 1 ? "Entrega em até $delivery dias úteis" : 'Entrega em 1 dia útil';
-						$label_suffix = apply_filters(
-							'wc_shipping_simulator_integration_correios_rate_label_suffix',
-							" ($time)",
-							$rate
-						);
-						if ( $label_suffix ) {
-							$rate->set_label( $rate->get_label() . $label_suffix );
-						}
+						$label = wc_correios_get_estimating_delivery( $rate->get_label(), $delivery );
+						$rate->set_label( $label );
 					}
 				}
 			}
