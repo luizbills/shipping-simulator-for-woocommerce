d456077180a02121b82a2a737003fd37df48daef
Luiz Bills <luizbills@gmail.com>
2022-08-01 11:50:31 -0300

    check woocommerce_ship_to_countries option

--- dependencies.php
+++ dependencies.php
@@ -16,7 +16,7 @@ return [
 		'check' => function () {
 			$req_version = h::config_get( 'REQUIRED_PHP_VERSION', false );
 			$serv_version = \preg_replace( '/[^0-9\.]/', '', PHP_VERSION );
-			return $req_version && $serv_version ? \version_compare( $serv_version, $req_version, '>=' ) : true;
+			return ( $req_version && $serv_version ) ? \version_compare( $serv_version, $req_version, '>=' ) : true;
 		}
 	],
 
@@ -29,4 +29,14 @@ return [
 			return \function_exists( 'WC' );
 		}
 	],
+
+	'woocommerce-shipping' => [
+		'message' => sprintf(
+			__( 'The WooCommerce option %s is disabled.', 'wc-shipping-simulator' ),
+			'<strong>' . esc_html__( 'Shipping location(s)', 'wc-shipping-simulator' ) . '</strong>'
+		),
+		'check' => function () {
+			return 'disabled' !== get_option( 'woocommerce_ship_to_countries' );
+		}
+	]
 ];
