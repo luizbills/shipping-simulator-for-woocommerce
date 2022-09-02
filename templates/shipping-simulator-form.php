1c683d5f2b2f944238dd373ee53d82eb66933aa2
Luiz Bills <luizbills@pm.me>
2022-09-02 14:11:27 -0300

    improve a11y

--- templates/shipping-simulator-form.php
+++ templates/shipping-simulator-form.php
@@ -8,11 +8,11 @@
 		<div id="wc-shipping-sim-form-fields">
 			<?php do_action( 'wc_shipping_simulator_form_fields_start' ) ?>
 
-			<input name="postcode" type="<?php echo esc_attr( $input_type ); ?>" value="<?php echo esc_attr( $input_value ) ?>" placeholder="<?php echo esc_attr( $input_placeholder ); ?>" title="<?php echo esc_attr( $input_placeholder ); ?>" class="input-text input-postcode" required>
+			<input name="postcode" type="<?php echo esc_attr( $input_type ); ?>" value="<?php echo esc_attr( $input_value ) ?>" placeholder="<?php echo esc_attr( $input_placeholder ); ?>" aria-label="<?php echo esc_attr( $input_placeholder ); ?>" class="input-text input-postcode" required>
 
 			<?php echo $nonce ?>
 
-			<button type="submit" class="button submit"><?php echo esc_html( $submit_label ); ?></button>
+			<button type="submit" class="button submit" aria-label="<?php esc_attr_e( 'Calculate shipping', 'wc-shipping-simulator' ); ?>"><?php echo esc_html( $submit_label ); ?></button>
 
 			<?php do_action( 'wc_shipping_simulator_form_fields_end' ) ?>
 		</div>
