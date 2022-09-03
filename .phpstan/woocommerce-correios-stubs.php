09162c81a3f8977fa270ab6a7b2e681d80874a82
Luiz Bills <luizbills@pm.me>
2022-09-03 18:09:15 -0300

    add stubs for wc correios plugin

---
+++ .phpstan/woocommerce-correios-stubs.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace {
+	class WC_Correios_Autofill_Addresses {
+
+		/**
+		 * @param string $postcode
+		 * @return stdClass
+		 */
+		public static function get_address( $postcode ) {
+		}
+	}
+
+	/**
+	 * @param string $name
+	 * @param string|int $days
+	 * @param int $additional_days
+	 * @return string
+	*/
+	function wc_correios_get_estimating_delivery( $name, $days, $additional_days = 0 ) {
+	}
+}
