13f2352aded34dabce0c485bbfe32f090396c78b
Luiz Bills <luizbills@pm.me>
2023-02-02 11:55:25 -0300

    format loader

--- loader.php
+++ loader.php
@@ -5,17 +5,17 @@ namespace Shipping_Simulator;
 defined( 'WPINC' ) || exit( 1 );
 
 return [
-	Ajax::class,
+	Admin\Settings::class,
+	Admin\Plugin_Meta::class,
+	Admin\Notices::class,
+	Integration\Brazil::instance(),
+	Integration\Correios::instance(),
+	Integration\Free_Shipping::instance(),
+	Integration\Melhor_Envio::instance(),
+	Request::class,
 	Shortcode::class,
 	Tweaks::class,
 	Logger::class,
 	Integrations::class,
 	Debug_Box::class,
-	Integration\Brazil::class,
-	Integration\Correios::class,
-	Integration\Free_Shipping::class,
-	Integration\Melhor_Envio::instance(),
-	Admin\Settings::class,
-	Admin\Plugin_Meta::class,
-	Admin\Notices::class,
 ];
